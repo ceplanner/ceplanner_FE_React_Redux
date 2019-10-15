fc2074a609c95a078cf35fe2d0a9f6e27b6aaef4
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-10-15 18:53:15 -0400

    fixed add event pages

--- src/Components/MultistepEventAdd/EventDetails.js
+++ src/Components/MultistepEventAdd/EventDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import {  Form, FormGroup, Label, Input, Button } from "reactstrap";
-
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import "./addEvent.css";
 class EventDetails extends Component {
   saveAndContinue = e => {
     e.preventDefault();
@@ -10,58 +10,44 @@ class EventDetails extends Component {
   render() {
     const { values } = this.props;
     return (
-      <Form color="green">
-        <h1 className="centered">When New Ideas Start</h1>
-        
-            <FormGroup>
-              <Label>Event Name</Label>
-              <Input
-                placeholder="Event Name"
-                onChange={this.props.handleChange("eventName")}
-                defaultValue={values.eventName}
-              />
-            </FormGroup>
-         
-            <FormGroup>
-              <Label>Event Type</Label>
-              <Input
-                type="text"
-                placeholder="Event Type"
-                onChange={this.props.handleChange("eventType")}
-                defaultValue={values.eventType}
-              />
-            </FormGroup>
-         
-     
-        <FormGroup>
-          <Label>Event Start Date</Label>
-          <Input
-            type="date"
-            placeholder="Event Start Date"
-            onChange={this.props.handleChange("eventDate")}
-            defaultValue={values.eventDate}
-          />
-        </FormGroup>
-        {/* 
-                <FormGroup>
-                    <Label>Password</Label>
-                    <Input
-                    type='password'
-                    placeholder='Password'
-                    onChange={this.props.handleChange('password')}
-                    defaultValue={values.password}
-                    />
-                </FormGroup> */}
-        {/* <FormGroup>
-                    <Label>Confirm Password</Label>
-                    <Input
-                    type='password'
-                    placeholder='Confirm Password'
-                    onChange={this.props.handleChange('confirmPassword')}
-                    defaultValue={values.confirmPassword}
-                    />
-                </FormGroup> */}
-        <Button onClick={this.saveAndContinue}>Save And Continue </Button>
+      <Form id='form' color='green'>
+        <div className='formContainer'>
+          <h2 id='center'>When New Ideas Start</h2>
+          <FormGroup>
+            <Label>Event Name</Label>
+            <Input
+              id='input'
+              placeholder='Event Name'
+              onChange={this.props.handleChange("eventName")}
+              defaultValue={values.eventName}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label>Event Type</Label>
+            <Input
+              id='input'
+              type='text'
+              placeholder='Event Type'
+              onChange={this.props.handleChange("eventType")}
+              defaultValue={values.eventType}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label>Event Start Date</Label>
+            <Input
+              id='input'
+              type='date'
+              placeholder='Event Start Date'
+              onChange={this.props.handleChange("eventDate")}
+              defaultValue={values.eventDate}
+            />
+          </FormGroup>
+          <Button id='btn' onClick={this.saveAndContinue}>
+            Save And Continue{" "}
+          </Button>
+        </div>
       </Form>
     );
   }
--- src/Components/MultistepEventAdd/EventMoreDetails.js
+++ src/Components/MultistepEventAdd/EventMoreDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
-
+import "./addEvent.css";
 class EventMoreDetails extends Component {
   saveAndContinue = e => {
     e.preventDefault();
@@ -15,43 +15,51 @@ class EventMoreDetails extends Component {
   render() {
     const { values } = this.props;
     return (
-      <Form color="blue">
-        <h1 className="ui centered">A little More About the Event</h1>
-        <FormGroup>
-          <Label>Description</Label>
-          <Input
-            type='text'
-            placeholder="Event Description"
-            onChange={this.props.handleChange("eventDescription")}
-            defaultValue={values.eventDescription}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label>Event Budget</Label>
-          <Input
-            placeholder="Event Budget"
-            onChange={this.props.handleChange("eventBudget")}
-            defaultValue={values.eventBudget}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label>Location</Label>
-          <Input
-            placeholder="Location"
-            onChange={this.props.handleChange("location")}
-            defaultValue={values.location}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label>Agenda</Label>
-          <Input
-            placeholder="Agenda"
-            onChange={this.props.handleChange("agenda")}
-            defaultValue={values.agenda}
-          />
-        </FormGroup>
-        <Button onClick={this.back}>Back</Button>
-        <Button onClick={this.saveAndContinue}>Save And Continue </Button>
+      <Form id='form' color='blue'>
+        <div className='formContainer'>
+          <h1 id='header'>A little More About the Event</h1>
+          <FormGroup>
+            <Label>Description</Label>
+            <Input
+              id='input'
+              type='text'
+              placeholder='Event Description'
+              onChange={this.props.handleChange("eventDescription")}
+              defaultValue={values.eventDescription}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Event Budget</Label>
+            <Input
+              id='input'
+              placeholder='Event Budget'
+              onChange={this.props.handleChange("eventBudget")}
+              defaultValue={values.eventBudget}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Location</Label>
+            <Input
+              id='input'
+              placeholder='Location'
+              onChange={this.props.handleChange("location")}
+              defaultValue={values.location}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Agenda</Label>
+            <Input
+              id='input'
+              placeholder='Agenda'
+              onChange={this.props.handleChange("agenda")}
+              defaultValue={values.agenda}
+            />
+          </FormGroup>
+          <div className='flexBtn'>
+            <Button onClick={this.back}>Back</Button>
+            <Button onClick={this.saveAndContinue}>Save And Continue </Button>
+          </div>
+        </div>
       </Form>
     );
   }
